fdf95a7089f0b983183129c72f6846906a9b5ffa
Lara Vieira <contato@laravieira.me>
2021-06-23 20:27:27 -0300

    Home Page layout created.

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,15 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/models/user_model.dart';
+import 'package:payflow/shared/auth/auth_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    UserModel user = AuthController.user;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Page"),
-        backgroundColor: AppColors.primary,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(152),
+        child: Container(
+          color: AppColors.primary,
+          height: 152,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                  text: "Olá, ",
+                  style: AppTextStyles.titleRegular,
+                  children: [
+                    TextSpan(
+                      text: user.name,
+                      style: AppTextStyles.titleBoldBackground,
+                    ),
+                  ],
+                ),
+              ),
+              subtitle: Text(
+                "Mantenha suas contas em dia : ]",
+                style: AppTextStyles.captionShape,
+              ),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Image.network(user.profile!),
+              ),
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.home),
+              color: AppColors.primary,
+            ),
+            Container(
+              height: 52,
+              width: 52,
+              decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(5)),
+              child: IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.add_box_outlined)),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.description_outlined),
+              color: AppColors.body,
+            ),
+          ],
+        ),
       ),
     );
   }
